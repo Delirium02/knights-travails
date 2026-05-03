6cfe331660e93229162649dbee6adcfef032ebcd
delirium02 <darknightbs@gmail.com>
2026-05-03 23:32:41 +1000

    Completed knightMoves function, working correctly

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const moves = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]];
 
 function knightMoves(start, end) {
+  const queue = [[start]];
+  const visited = new Set();
+  visited.add(start.toString());
 
+  while (queue.length > 0) {
+    let path = queue.shift();
+    let position = path[path.length - 1];
+
+    if (position[0] === end[0] && position[1] === end[1]) {
+      return `You made it in ${path.length - 1} moves!  Here's your path:\n${path.map(([x, y]) => `[${x}, ${y}]`).join('\n')}`;
+    }
+
+    let nextMoves = getPossibleMoves(position);
+
+    for (const move of nextMoves) {
+      const key = move.toString();
+
+      if (!visited.has(key)) {
+        queue.push([...path, move]);
+        visited.add(key);
+      }
+    }
+  }
+
+  return "No path found.";
 }
 
 function getPossibleMoves(position) {
@@ -17,6 +41,14 @@ function getPossibleMoves(position) {
     }
   }
 
-  return possibleMoves;
+  return `Possible moves from ${position}: ${possibleMoves.map(([x, y]) => `[${x}, ${y}]`).join(', ')}`;
 }
+
+// Example usage of the knightMoves function:
+console.log(knightMoves([0, 0], [7, 7]));
+console.log(knightMoves([0, 0], [1, 2]));
+
+// Example usage of the getPossibleMoves function:
+console.log(getPossibleMoves([0, 0]));
+console.log(getPossibleMoves([4, 4]));
 
